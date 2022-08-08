d5f17f626c553830a3eb558629a7bfc820c4d8fb
Serg_Sytnikov <tlingaal1996@gmail.com>
2022-08-08 17:00:42 +0300

    add random numers

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,5 +1,12 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);//1,2,3,.....9
+int numberB = new Random().Next(1,10);
+
+/*int numberA = 31;
+int numberB = 5;*/
+
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+
 //Console.WriteLine(numberA + numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
